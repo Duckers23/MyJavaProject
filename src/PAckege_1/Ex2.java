8e94a74ed306e30bec399449ba5ebf7a9c418ec8
maksym <mikhailishi.maks@gmail.com>
2023-05-11 18:54:41 +0200

    Initial commit

---
+++ src/PAckege_1/Ex2.java
@@ -0,0 +1,12 @@
+package PAckege_1;
+
+public class Ex2 {
+    //psvm + Enter = public static void main(String[] args){}
+    public static void main(String[] args) {
+        System.out.println();
+        //коментарій sout + Enter = System.out.println();
+        //коментарій CTRL + "/" = commentarii
+
+
+    }
+}
